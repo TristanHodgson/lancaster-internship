bae9993f812eb780680296a15bfa0caf2473dca2
Tristan Hodgson <hello@tristanhodgson.com>
2026-07-17 09:04:23 +0100

    Adding policy sum

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 class MDP:
     def __init__(self, actions, gamma):
-        self.actions = actions
+        self.transitions = actions
         self.gamma = gamma # The discount factor
 
     def states(self):
         # Return the set of all states
-        return self.actions.keys()
+        return self.transitions.keys()
 
     def actions(self, state):
         # For a given state, return a list of possible actions
-        return self.actions[state].keys()
+        return self.transitions[state].keys()
 
     def outcomes(self, state, action):
         # For a given state and action, return a list of possible outcomes
-        return self.actions[state][action]
+        return self.transitions[state][action]
 
     def is_terminal(self, state):
         # For a given state, return True if the state is terminal
-        return len(self.actions[state]) == 0
+        return len(self.transitions[state]) == 0
 
     def terminal_states(self):
         # Return the set of all terminal states
         return {state for state in self.states() if self.is_terminal(state)}
-    
 
-# Dictionary of states:[action: [(probability, next_state, reward)]]
+
+def policy_sum(mdp, state, action, V):
+    # Computes \sum_{s',r} p(s',r|s,a) (r + \gamma V(s')) for all actions a in state s
+    return sum(prob * (reward + mdp.gamma * V[next_state])
+        for prob, next_state, reward in mdp.outcomes(state, action))
+
+
+# Dictionary of states:{state:[(probability, next_state, reward)]}
+# Must be stochastic, i.e. the sum of the probabilities for each state, action pair must be 1
 actions = {
     "s0": {
         "left": [(0.8, "s1", 2), (0.2, "s3", -1)],
-        "right": [(1.0, "s3", 0)]
+        "right": [(0.3, "s3", 0), (0.7, "s3", 1)]
     },
     "s1": {
         "finish": [(1.0, "s3", 5)]
@@ -36,7 +43,15 @@ actions = {
     "s3": {}
 }
 
+# Dictionary of states:{action: probability}
+# This allows us to have both stochastic and deterministic policies
+policy = {
+    "s0": {"left": 0.5, "right": 0.5},
+    "s1": {"finish": 1.0},
+}
+
+
 mdp = MDP(actions=actions, gamma=0.9)
-print(mdp.terminal_states())
-print(mdp.actions["s0"])
-print(mdp.outcomes("s0", "left"))
+
+
+V = {state: 0 for state in mdp.states()}
