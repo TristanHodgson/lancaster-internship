ff18db0b0cd4d0fb9f56e59db1153ad73cf2911a
Tristan Hodgson <hello@tristanhodgson.com>
2026-07-28 15:39:58 +0100

    Add Gurobi LP solver

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from modules.mdp import *
 from modules.policy_iteration import policy_iteration
 from modules.value_iteration import value_iteration
 from modules.helper import action_from_state, greedy_policy, all_close, graph_policy, get_max_action
+from modules.lp import lp
+
+
+########################
+###  Gurobi License  ###
+########################
+
+import os
+os.environ["GRB_LICENSE_FILE"] = "secret/gurobi.lic"
 
 
 ########################
@@ -42,6 +51,13 @@ actions = generate_mdp(**PARAMS)
 mdp = MDP(actions=actions, gamma=PARAMS["gamma"])
 initial_policy = greedy_policy(mdp)
 PI_policy, PI_V = policy_iteration(mdp, initial_policy, EPSILON)
+LP_policy = lp(mdp)
+
+# assert PI_policy == LP_policy, "Policy Iteration and LP did not converge to the same policy"
+
+graph_policy(mdp, LP_policy, PARAMS["N"])
+graph_policy(mdp, PI_policy, PARAMS["N"])
+
 # VI_policy, VI_V = value_iteration(mdp, THETA)
 # assert all_close(PI_V, VI_V, tol=TOL), "Policy Iteration and Value Iteration did not converge to the same value function"
 # graph_policy(mdp, PI_policy, PARAMS["N"])
@@ -94,20 +110,20 @@ print(f"Time taken for value iteration: {end - start:.4f} seconds")
 ###    Experiment    ###
 ########################
 
-Ns = [50]
-Ps = [i for i in range(1, 10)] + [10**i for i in range(4,12, 4)]
+# Ns = [50]
+# Ps = [i for i in range(1, 10)] + [10**i for i in range(4,12, 4)]
 
-table_data = []
-for N in Ns:
-    for P in tqdm(Ps):
-        PARAMS["N"] = N
-        PARAMS["p"] = P * N
-        PARAMS["delta"] = 1 / (PARAMS["N"] * PARAMS["tau"])
-        actions = generate_mdp(**PARAMS)
-        mdp = MDP(actions=actions, gamma=PARAMS["gamma"])
-        initial_policy = greedy_policy(mdp)
-        PI_policy, PI_V = policy_iteration(mdp, initial_policy, EPSILON)
-        graph_policy(mdp, PI_policy, N, title=f"Policy Heatmap for N={N}, P={P * N }, tau={PARAMS['tau']}, gamma={PARAMS['gamma']}", SAVE=True, filename=f"gamma{PARAMS['gamma']}_N{N}_P{P * N}_t{PARAMS['tau']}")
-        table_data.append([N, P * N, get_max_action(PI_policy)[0], get_max_action(PI_policy)[1]])
+# table_data = []
+# for N in Ns:
+#     for P in tqdm(Ps):
+#         PARAMS["N"] = N
+#         PARAMS["p"] = P * N
+#         PARAMS["delta"] = 1 / (PARAMS["N"] * PARAMS["tau"])
+#         actions = generate_mdp(**PARAMS)
+#         mdp = MDP(actions=actions, gamma=PARAMS["gamma"])
+#         initial_policy = greedy_policy(mdp)
+#         PI_policy, PI_V = policy_iteration(mdp, initial_policy, EPSILON)
+#         graph_policy(mdp, PI_policy, N, title=f"Policy Heatmap for N={N}, P={P * N }, tau={PARAMS['tau']}, gamma={PARAMS['gamma']}", SAVE=True, filename=f"gamma{PARAMS['gamma']}_N{N}_P{P * N}_t{PARAMS['tau']}")
+#         table_data.append([N, P * N, get_max_action(PI_policy)[0], get_max_action(PI_policy)[1]])
 
-print(tabulate.tabulate(table_data, headers=["N", "P", "Max Action State", "Max Action"], tablefmt="github"))
+# print(tabulate.tabulate(table_data, headers=["N", "P", "Max Action State", "Max Action"], tablefmt="github"))
---
+++ modules/lp.py
@@ -0,0 +1,71 @@
+import pulp
+from modules.mdp import MDP
+
+
+def solve_lp(mdp):
+    # maximises \sum_{s \in S} \sum_{a \in A(s)} r(s, a) x_{s, a}
+    # Subject to:
+    #   \sum_{a \in A(j)} x_{j, a} - \gamma \sum_{s \in S} \sum_{a \in A(s)} P(j | s, a) x_{s, a} = \alpha_j \quad \forall j \in S 
+    #   x_{s, a} \ge 0 \quad \forall s \in S, \forall a \in A(s)
+    prob = pulp.LpProblem("MDP_LP_Discounted", pulp.LpMaximize)
+    x = {(s, a): pulp.LpVariable(f"x_{s[0]}_{s[1]}_{a}", lowBound=0) for s in mdp.states() for a in mdp.actions(s)}
+    alpha = 1.0 / len(list(mdp.states())) # vector that has to be positive, stochastic; we simplify to a scalar
+
+    objective = []
+    condition_term2 = {s: [] for s in mdp.states()}
+    for s in mdp.states():
+        for a in mdp.actions(s):
+            expected_reward = sum(p * r for p, _, r in mdp.outcomes(s, a)) # r(s,a)
+            for p, next_s, _ in mdp.outcomes(s, a):
+                condition_term2[next_s].append(p * x[(s, a)]) # P(j | s, a) x_{s, a}, we only consider the states j with non-zero probability of being reached, hence use the set of outcomes
+            objective.append(expected_reward * x[(s, a)]) # r(s,a) * x_{s,a}
+    prob += pulp.lpSum(objective) # Set the objective as the sum of the elements of the list
+
+    for j in mdp.states():
+        condition_term1 = pulp.lpSum(x[(j, a)] for a in mdp.actions(j)) # \sum_{a \in A(j)} x_{j, a}
+        prob += (condition_term1 - mdp.gamma * pulp.lpSum(condition_term2[j]) == alpha) # Adding the whole of the first condition
+
+    prob.solve(pulp.GUROBI(msg=False)) # Solve using Gurobi
+    return {s: {max(mdp.actions(s), key=lambda a: pulp.value(x[(s, a)]) or 0): 1.0} for s in mdp.states()} # Produce a deterministic policy by taking the action with the highest x(s,a) value for each state s, or the first if all 0
+
+
+def solve_lp_gamma_1(mdp):
+    # maximises \sum_{s \in S} \sum_{a \in A(s)} r(s, a) x_{s, a}
+    # Subject to:
+    #   \forall j\in S \qquad \sum_a x_{j, a} - \sum_{s \in S} \sum_{a \in A(s)} P(j | s, a) x_{s, a} = 0
+    #   \forall j \in S \qquad \sum_{a \in A(j)} x_{j, a} + \sum_{a\in A(j)} y_{j, a} - \sum_{s \in S} \sum_{a \in A(s)} P(j | s, a) y_{s, a} = \alpha_j
+    #   x_{s, a} \ge 0 \quad \forall s \in S, \forall a \in A(s)
+    #   y_{s, a} \ge 0 \quad \forall s \in S, \forall a \in A(s)
+    prob = pulp.LpProblem("MDP_LP_Gamma_1", pulp.LpMaximize)
+    x = {(s, a): pulp.LpVariable(f"x_{s[0]}_{s[1]}_{a}", lowBound=0) for s in mdp.states() for a in mdp.actions(s)}
+    y = {(s, a): pulp.LpVariable(f"y_{s[0]}_{s[1]}_{a}", lowBound=0) for s in mdp.states() for a in mdp.actions(s)}
+    alpha = 1.0 / len(list(mdp.states())) # vector that just has to be positive, stochastic; we simplify to a scalar
+
+    objective = []
+    condition1_term2 = {s: [] for s in mdp.states()}
+    condition2_term2 = {s: [] for s in mdp.states()}
+    for s in mdp.states():
+        for a in mdp.actions(s):
+            expected_reward = sum(p * r for p, _, r in mdp.outcomes(s, a)) # r(s,a)
+            objective.append(expected_reward * x[(s, a)]) # r(s,a) * x_{s,a}
+            for p, next_s, _ in mdp.outcomes(s, a):
+                condition1_term2[next_s].append(p * x[(s, a)]) # working out P(j | s, a) x_{s, a}, we only consider the states j with non-zero probability of being reached
+                condition2_term2[next_s].append(p * y[(s, a)]) # working out P(j | s, a) y_{s, a}, we only consider the states j with non-zero probability of being reached
+    prob += pulp.lpSum(objective) # Set the objective as the sum of the elements of the list
+
+    for j in mdp.states():
+        condition1_term1 = pulp.lpSum(x[(j, a)] for a in mdp.actions(j)) # \sum_{a \in A(j)} x_{j, a}
+        condition2_term1 = pulp.lpSum(x[(j, a)] + y[(j, a)] for a in mdp.actions(j)) # \sum_{a \in A(j)} x_{j, a} + \sum_{a\in A(j)} y_{j, a}
+        prob += (condition1_term1 - pulp.lpSum(condition1_term2[j]) == 0) # Adding the whole of the first condition
+        prob += (condition2_term1 - pulp.lpSum(condition2_term2[j]) == alpha) # Adding the whole of the second condition
+
+    prob.solve(pulp.GUROBI(msg=False)) # Solve using Gurobi
+    return {s: {max(mdp.actions(s), key=lambda a: pulp.value(x[(s, a)]) or 0): 1.0} for s in mdp.states()} # Produce a deterministic policy by taking the action with the highest x(s,a) value for each state s, or the first if all 0
+
+
+
+def lp(mdp):
+    if mdp.gamma == 1:
+        return solve_lp_gamma_1(mdp)
+    else:
+        return solve_lp(mdp)
